2e68c7c9e11fcf6471f4dd6267f9a10a1f291426
Janice Teguh <janicejarusdy@gmail.com>
2022-11-29 20:50:21 +1100

    passed all tests

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { movies } from "../data";
 
+// {
+//   title: "Doctor Strange",
+//   time: 115,
+//   genres: ["Action", "Adventure", "Fantasy"],
+// }
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie=>{
+        return(
+          <div key={movie.title}>
+            Title: {movie.title}<br/>
+            Time: {movie.time}
+            <ul>{movie.genres.map(genre=><li key={genre}>{genre}</li>)}</ul>
+          </div>
+        )
+      })}
+
+    </div>
+  );
 }
 
 export default Movies;
